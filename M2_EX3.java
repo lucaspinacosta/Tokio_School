763c5bf7dd199433b0fa7bfc6292dde7f5cc7195
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-05 11:12:59 +0100

    Create M2_EX3.java

---
+++ M2_EX3.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pao de Queijo(Lucas Costa)
+ */
+
+
+public class M2_EX3 {
+
+
+
+     class Programa{
+         c
+               Conta minhaConta;
+               minhaConta = new Conta();
+
+               minhaConta.titular = "Duke";
+               minhaConta.numero_de_conta = 15489;
+               minhaConta.quantidade = 150.50;
+
+
+               System.out.println("Saldo Atual:" + minhaConta.Saldo);
+
+          }
+     }
+
+
+     class Conta{
+
+          String titular;
+          int numero_de_conta;
+          double quantidade;
+     }
+
+     
+
+
+
+
+
+     
+}
+
+
