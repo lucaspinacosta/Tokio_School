61522dc196abb45eda5b49c4528dd6575ebbd90e
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-06 14:15:15 +0100

    Update M2_EX2.java

--- M2_EX2.java
+++ M2_EX2.java
@@ -1,28 +1,37 @@
 import java.io.IOException;
+import java.lang.invoke.VarHandle;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextPane;
 
 public class M2_EX2 {
 
     public static void main(String args[]) throws IOException {
 
+        
         String senha = "paodequeijo";
-
+        
         int tentativas = 0;
         int chances = 3;
 
         while (tentativas < chances) {
+            
             chances--;
-            String inpsenha = JOptionPane.showInputDialog("Digite a senha");
-            if (senha == inpsenha) {
-                JOptionPane.showMessageDialog(null, "Senha correta", inpsenha, JOptionPane.PLAIN_MESSAGE);
+            String ippsenha = JOptionPane.showInputDialog(null, "senha");
+            
+            if (ippsenha == senha) {
+                JOptionPane.showMessageDialog(null, "Senha correta", ippsenha, JOptionPane.PLAIN_MESSAGE);
                 return;
             } else {
-                JOptionPane.showMessageDialog(null, "Senha Errada", inpsenha, JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Senha Errada", "Senha inserida " + ippsenha, JOptionPane.PLAIN_MESSAGE);
             }
 
-        }
-        JOptionPane.showMessageDialog(null, "Ficou sem tentativas");
+        }JOptionPane.showMessageDialog(null, "Ficou sem tentativas");
+        return;
+        
+        
+        
 
     }
 
