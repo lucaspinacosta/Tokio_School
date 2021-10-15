6b46a0aaa0874b088a78008ec6698a36e603af85
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-15 16:27:25 +0100

    Create M2_EX6.java

---
+++ M2_EX6.java
@@ -0,0 +1,19 @@
+////////////////////////////
+/// @power_by paodequeijo///
+////////////////////////////
+
+public class M2_EX6 {
+
+    public static void main(String[] args) {
+        int numero = 0;
+        int multiplicador = 9;
+        while (numero < 10) {
+            numero++;
+            int total = multiplicador * numero;
+            System.out.println(multiplicador + "X" + numero + "=" + total);
+
+        }
+
+    }
+
+}
