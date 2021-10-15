58574a11a7606790d76e3f40aeb666742cb3e80f
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-15 12:58:19 +0100

    update

--- M2_EX3.java
+++ M2_EX3.java
@@ -18,49 +18,42 @@ public class M2_EX3 {
 
 }
      class Conta{
-                    String titular;
-                    int numero_conta;
-                    double quantidade;
-               
+                    private String titular;
+                    private int numero_conta;
+                    private double quantidade;
+
+                    public Conta(String titular,int numero_conta,  double quantidade){
+                         this.titular = titular;
+                         this.numero_conta = numero_conta;
+                         this.quantidade = quantidade;
+
+                    }
+                    public String getTitular() {  return titular; }
+                    public int getNumero_conta() { return numero_conta; }
+                    public double getQuantidade() { return quantidade; }
+
+                    public void setTitular(String titular) {this.titular = titular;}
+                    public void setNumero_conta(int numero_conta) { this.numero_conta = numero_conta;}
+                    public void setQuantidade(double quantidade) { this.quantidade = quantidade;}
      
 
+
      public static void main(String[] args) {
+
           
-
-
-               Conta minhaConta1;
-               minhaConta1 = new Conta();
-
-               minhaConta1.titular = "Duke";
-               minhaConta1.quantidade = 1500.50;
-               minhaConta1.numero_conta = 54612;
-
-
-               Conta minhaConta2;
-               minhaConta2 = new Conta();
-
-               minhaConta2.titular = "Irene";
-               minhaConta2.numero_conta = 54156;
-               minhaConta2.quantidade = 15464.54;
-
-               if (minhaConta1.quantidade > minhaConta2.quantidade){
-                    System.out.printf(
-                         "Titular: " + minhaConta1.titular + '\n' +
-                         "Quantidade: "+ minhaConta1.quantidade +"€" + '\n'+
-                         "Numero de Conta: " + minhaConta1.numero_conta 
-                    );
-               } else {
-                    System.out.println(
-                         "Titular: " + minhaConta2.titular + '\n'+
-                         "Quantia: " + minhaConta2.quantidade + "€" + '\n'+
-                         "Numero de Conta: " + minhaConta2.numero_conta
-                         );
-               }
-
+          Conta minhaConta1;
+          minhaConta1 = new Conta("Irene", 347854, 32784.23);
+          
+          Conta minhaConta2;
+          minhaConta2 = new Conta("Pão de Queijo", 982457, 100000.99);
                
-
           
-          
+          if (minhaConta1.quantidade < minhaConta2.quantidade){
+               System.out.printf("Titular:\s"+minhaConta2.titular+'\n'+"ID:\s"+minhaConta2.numero_conta +'\n'+ "Quantidade:\s"+minhaConta2.quantidade+"€");
+               
+          }else{
+               System.out.printf("Titular:\s"+minhaConta1.titular+'\n'+"ID:\s"+minhaConta1.numero_conta+'\n'+"Quantidade:\s"+minhaConta1.quantidade+"€");
+          }
 
           
      
