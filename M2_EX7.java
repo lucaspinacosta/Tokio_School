a0c9a0a804f60c455d2e7212fc36f1263341e9b0
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-18 10:54:43 +0100

    new

--- M2_EX7.java
+++ M2_EX7.java
@@ -1,25 +1,28 @@
+////////////////////////////
+/// @power_by paodequeijo///
+////////////////////////////
 
 import javax.swing.JOptionPane;
 
 public class M2_EX7 {
      public static void main(String[] args) {
-          
+
           String nota_aluno = JOptionPane.showInputDialog("Insira a nota do aluno (0/10)");
           int nota_de_ava = Integer.parseInt(nota_aluno);
 
-          if (nota_de_ava <= 2){
+          if (nota_de_ava <= 2) {
                JOptionPane.showMessageDialog(null, "Insuficiente", "Nota do Aluno", JOptionPane.INFORMATION_MESSAGE);
           }
-          if (nota_de_ava >= 3 && nota_de_ava <= 5){
+          if (nota_de_ava >= 3 && nota_de_ava <= 5) {
                JOptionPane.showMessageDialog(null, "Suficiente", "Nota do Aluno", JOptionPane.INFORMATION_MESSAGE);
           }
-          if (nota_de_ava >= 6 && nota_de_ava <=7){
+          if (nota_de_ava >= 6 && nota_de_ava <= 7) {
                JOptionPane.showMessageDialog(null, "Bom", "Nota do Aluno", JOptionPane.INFORMATION_MESSAGE);
           }
-          if (nota_de_ava >= 8 && nota_de_ava <=9){
+          if (nota_de_ava >= 8 && nota_de_ava <= 9) {
                JOptionPane.showMessageDialog(null, "Notalvel", "Nota do Aluno", JOptionPane.INFORMATION_MESSAGE);
           }
-          if (nota_de_ava == 10){
+          if (nota_de_ava == 10) {
                JOptionPane.showMessageDialog(null, "Muito Bom", "Nota Do Aluno", JOptionPane.INFORMATION_MESSAGE);
           }
      }
--- M2_EX8.java
+++ M2_EX8.java
@@ -1,17 +1,27 @@
+////////////////////////////
+/// @power_by paodequeijo///
+////////////////////////////
+
 import javax.swing.JOptionPane;
 
 public class M2_EX8 {
      public static void main(String[] args) {
-        
-        
-          String numero = JOptionPane.showInputDialog(null, "Insira um numero");
-        
-          int verif_num = Integer.parseInt(numero);
-
-          
-
-
-
-
+          int primo = Integer.parseInt(JOptionPane.showInputDialog(null, "insira um numero"));
+          boolean isPrimo = true;
+          int divisor = 0;
+          for (int i = 2; i <= primo; i++) {
+               if (((primo % i) == 0) && (i != primo)) {
+                    isPrimo = false;
+                    divisor = i;
+                    break;
+               }
+          }
+          if (isPrimo) {
+               JOptionPane.showMessageDialog(null, "O numero\s" + primo + "\s e Primo", "Primo",
+                         JOptionPane.INFORMATION_MESSAGE);
+          } else {
+               JOptionPane.showMessageDialog(null, "O numero \s" + primo + " nao e Primo", "Nao Primo",
+                         JOptionPane.INFORMATION_MESSAGE);
+          }
      }
 }
