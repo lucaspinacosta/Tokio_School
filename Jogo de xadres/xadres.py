a98509c34330cc04063a54bd34e75c43d7091fa5
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-21 15:29:24 +0100

    upp

--- Jogo de xadres/xadres.py
+++ Jogo de xadres/xadres.py
@@ -14,33 +14,91 @@ w.pack()
 
 
 
+canvas_vert= 100
+canvas_hori = 100
+
+def casas_brancas_a1():
+     w.create_rectangle (1, 1,canvas_vert, canvas_hori, fill = "mint cream")
+     w.grid(row=0,column=0)
+
+     w.create_rectangle(200,1,canvas_vert,canvas_hori,fill="black")
+     w.grid(row=0,column=1)
+
+     w.create_rectangle(300,1, canvas_vert+100,canvas_hori, fill="mint cream")
+     w.grid(row=0,column=2)
+
+     w.create_rectangle(400,1,canvas_hori+200,canvas_vert,fill="black")
+     w.grid(row=0,column=3)
+
+     w.create_rectangle(500,1,canvas_hori+300,canvas_vert,fill="mint cream")
+     w.grid(row=0,column=4)
+
+     w.create_rectangle(600,1,canvas_hori+400,canvas_vert,fill="black")
+     w.grid(row=0,column=5)
+
+     w.create_rectangle(700, 1, canvas_hori+500,canvas_vert,fill="mint cream")
+     w.grid(row=0,column=6)
+
+     w.create_rectangle(800,1,canvas_hori+600, canvas_vert,fill="black")
+     w.grid(row=1,column=7)
+
+     w.create_rectangle(900, 1, canvas_hori+700,canvas_vert, fill="mint cream")
+     w.grid(row=1, column=8)
 
 
-def casas_brancas():
-     casa_branca = int (canvas_vert/2)
-     w.create_rectangle (1, 100,canvas_vert, canvas_hori+100, fill = "mint cream")
-     w.create_rectangle (100, 1, canvas_vert+100, canvas_hori+100,fill ="black")
-     w.grid()
+def casas_pretas_b1():
+     w.create_rectangle (1, 200, canvas_vert,canvas_hori, fill= "black")
+     w.grid(row=1,column=0)
+
+     w.create_rectangle (200,200, canvas_vert,canvas_hori, fill= "mint cream")
+     w.grid(row=1,column=1)
+
+     w.create_rectangle(300,200,canvas_vert+100,canvas_hori,fill="black")
+     w.grid(row=1,column=2)
+
+     w.create_rectangle(400,200,canvas_hori+200,canvas_vert,fill="mint cream")
+     w.grid(row=1,column=3)
+
+def casas_brancas_c1():
+     w.create_rectangle(1,300,canvas_vert,canvas_hori+100,fill="mint cream")
+     w.grid(row=2,column=0)
+     
+
+     w.create_rectangle(200,300,canvas_vert,canvas_hori+100,fill="black")
+     w.grid(row=2,column=1)
+
+
+     w.create_rectangle(300,300,canvas_vert+100,canvas_hori+100,fill="mint cream")
+     w.grid(row=2,column=2)
+
+def casas_pretas_d1():
+     w.create_rectangle(1,400,canvas_vert,canvas_hori+200,fill="black")
+     w.grid(row=3,column=0)
+
+
+     w.create_rectangle(200,400,canvas_vert,canvas_hori+200,fill="mint cream")
+     w.grid(row=3,column=1)
+
+
+     w.create_rectangle(300,400,canvas_vert+100,canvas_hori+200,fill="black")
+     w.grid(row=3,column=2)
 
 
 
-def coluna_vert():
-     for x in range(8):
-          canvas_vert= 100
-          canvas_hori = 100
+         
+         
+         
 
-          x_vert = int (1)
-          y_hori = int (1)
 
-          while y_hori < 800:
-               y_hori += 200
+def casas_geral():
+     casas_pretas_b1()
+     casas_brancas_a1()
+     casas_brancas_c1()
+     casas_pretas_d1()
+     
 
-               w.create_rectangle(x_vert,y_hori, canvas_vert, canvas_hori, fill = "mint cream")
-               w.create_rectangle(1, 200, canvas_vert, canvas_hori, fill = "black")
-               w.grid()
-               return;
+casas_geral()
 
-coluna_vert()
 
 
           
--- Jogo de xadres/xadres_0.1.py
+++ Jogo de xadres/xadres_0.1.py
@@ -2,19 +2,30 @@ from tkinter import *
 
 
 master = Tk()
-w = Canvas(master)
+w = Canvas(master, width=800, height=800)
+w.pack()
 
 
-colunas = 8
-linhas = 8
 
-casa_ver = 50
-casa_hor = 50
-tabela = []
 
-for i in range(linhas):
-     tabela.append(w.create_rectangle(1, 1, casa_ver,casa_hor))
 
-print (tabela)
+def tabuleiro():
+     i = 0
+     coluna_vertical = 1
+     coluna_horizontal = 1
+     medida_lateral = 100
+     medida_base = 100
+     numero_qudrados = 64
+     
+     for i in range (8):
+          w.create_rectangle(coluna_horizontal,coluna_vertical,medida_base,medida_lateral, fill="black")
+          w.grid()
+          coluna_horizontal+=200
+          i+=1
+          return
+
+     
+tabuleiro()
+         
 
 mainloop()
