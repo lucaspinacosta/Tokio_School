86a3440509098ff8583359a8b71a6203eec9c917
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-10-21 03:30:22 +0100

    ll

--- Jogo de xadres/xadres_0.1.py
+++ Jogo de xadres/xadres_0.1.py
@@ -3,34 +3,18 @@ from tkinter import *
 
 master = Tk()
 w = Canvas(master)
-master.geometry("800x800")
 
 
-casa_size_vert = 100
-casa_size_hori = 100
+colunas = 8
+linhas = 8
 
+casa_ver = 50
+casa_hor = 50
+tabela = []
 
+for i in range(linhas):
+     tabela.append(w.create_rectangle(1, 1, casa_ver,casa_hor))
 
-def casas_pretas():
-     w.create_rectangle(1, 1,casa_size_vert,casa_size_hori,fill= "black")
-     w.grid()
-
-def casas_brancas():
-     w.create_rectangle(1, 1, casa_size_vert, casa_size_hori, fill="mint cream")
-     w.grid()
-
-
-def coluna_vert():
-     y = 1
-     while y < 8:
-          casas_pretas
-          
-          y += 1
-
-          
-          return;
-     
-
-
+print (tabela)
 
 mainloop()
