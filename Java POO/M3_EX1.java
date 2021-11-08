6b488b252a5b87f16c4b742ada664636e2ff64ed
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-11-08 14:36:38 +0000

    sd

--- Java POO/M3_EX1.java
+++ Java POO/M3_EX1.java
@@ -1,3 +1,15 @@
+
 public class M3_EX1 {
+     class A {
+          static String nome = "Lucas";
+          static int idade = 27;
+
+          
+     }
      
+     
+     public static void main(String[] args) {
+
+     }
+
 }
