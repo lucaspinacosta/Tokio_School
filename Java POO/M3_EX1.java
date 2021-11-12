a9ff81d6e94bd54f3780c17c69e48f20775eebba
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-11-12 10:31:06 +0000

    upp

--- Java POO/M3_EX1.java
+++ Java POO/M3_EX1.java
@@ -1,36 +1,31 @@
 ////////////////////////////
 /// @power_by pãodequeijo///
 ////////////////////////////
+class A {
 
-public class M3_EX1 {
-     class A {
-          /// Agradecia tambem que me revisa-se a classe A, caso tenha algum erro, ou algo
-          /// que não
-          //// seja pedido no exercício
-          static String data_nasci(int dia, int mes, int ano) {
-               return (dia + "/" + mes + "/" + ano);
-          }
-
-          static String full_nome(String pr_nome, String ult_nome) {
-               return (pr_nome + "\s" + ult_nome);
-          }
-
-          private static int num_filhos(int quant_filhos) {
-               return (0);
-          }
-
-          private static int filhos_des = 1;
-
-          static int filhos_fut = num_filhos(0) + filhos_des;
-          static final String local_nasci = "Minas Gerais";
-
-          protected String loca_atual = "Guimarães";
-
+     static String data_nasci(int dia, int mes, int ano) {
+          return (dia + "/" + mes + "/" + ano);
      }
 
-     /// Class B{ *inicio aqui?*
+     static String full_nome(String pr_nome, String ult_nome) {
+          return (pr_nome + "\s" + ult_nome);
+     }
+
+     private static int num_filhos(int quant_filhos) {
+          return (0);
+     }
+
+     private static int filhos_des = 1;
+
+     static int filhos_fut = num_filhos(0) + filhos_des;
+     static final String local_nasci = "Minas Gerais";
+
+     protected String loca_atual = "Guimarães";
+}
+
+class B {
+
      public static void main(String[] args) {
-          /// class B { *ou inicio aqui?*
 
           System.out.println(A.full_nome("Lucas", "Costa"));
           System.out.println("Data de Nascimento: " + A.data_nasci(29, 04, 1994));
@@ -38,7 +33,4 @@ public class M3_EX1 {
           System.out.println("Numero desejado de filhos: " + A.filhos_fut);
 
      }
-     //// A minha duvida é se tenho de declarar a class B dentro do main, ou se tenho
-     //// de declarar a
-     /// class B fora da main e inclui-la dentro..//
 }
---
+++ Java POO/M3_ex3_1.java
@@ -0,0 +1,19 @@
+public class M3_ex3_1 {
+     /** criar 2 polimorficos */
+     class Veiculos {
+          Veiculos[] meusVeiculos = new Veiculos[3];
+     }
+
+     public void carro(String a, String b, String c) {
+
+     }
+
+     public void registro(int a, int b, int c) {
+
+     }
+
+     public static void main(String[] args) {
+          /** criar 3 polimorficos */
+     }
+
+}
