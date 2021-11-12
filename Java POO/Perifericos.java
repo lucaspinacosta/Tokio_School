8050dacc8ee07b84f1c06d1167b1e516c60694e3
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-11-12 14:51:06 +0000

    ups

---
+++ Java POO/Perifericos.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Perifericos {
+     static String Hdd(String marca, int valor) {
+          return (marca + "\s" + valor);
+     }
+
+     static String Modelo(String marca, int valor) {
+          return (marca + "\s" + valor);
+     }
+}
+
+class Insert_prod {
+     String hdd = Perifericos.Hdd(, valor);
+
+     public static void main(String[] args) {
+          String mas1 = JOptionPane.showInputDialog(null, "Nome do Produto:");
+     }
+
+}
