afadab55a43e1a4cfdf88f514fa0fc69a66c7dc7
Pão de Queijo <78106075+DonaIrene@users.noreply.github.com>
2022-04-19 18:55:02 +0100

    Projeto final under constriction

--- Python/Projeto Final/ProjetoFinal/app.py
+++ Python/Projeto Final/ProjetoFinal/app.py
@@ -1,1 +1,22 @@
-import 
+
+from flask import Flask,render_template,request,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+root = Flask(__name__)
+root.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/dados_informacoes.db'
+db=SQLAlchemy(root)
+root.config['DEBUG']=True
+
+class Produtos(db.Model):
+    __tablename__ = "produto"
+    numero_serie = db.Column(db.Integer,primary_key=True)
+    nome_de_produto = db.Column(db.String(100))
+    disponibilidade = db.Column(db.Boolean)
+    
+@root.route('/',methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+root.run()
