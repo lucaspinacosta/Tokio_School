ad3a10a73869a2133d2382d5cd4e1e8678118616
Pão de Queijo <78106075+DonaIrene@users.noreply.github.com>
2022-05-06 01:26:42 +0100

    changes

--- Python/Projeto Final/ProjetoFinal/app.py
+++ Python/Projeto Final/ProjetoFinal/app.py
@@ -38,8 +38,27 @@ class Clientes(db.Model):
     db.session.commit()
 
 
+#Verificar Usuario
+def db_verificar_email_admin(self):
+        con = sqlite3.connect('database/dados_informacoes.db')
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM admin")
+        emails = cursor.fetchall()
+        con.close()
+        return emails
+
+#Verificar Admin
+def db_verificar_email(self):
+        con = sqlite3.connect('database/dados_informacoes.db')
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM usuarios")
+        emails = cursor.fetchall()
+        con.close()
+        return emails
+
+
 # listagem dos produtos
-@root.route('/criar-produtos', methods=['GET', 'POST'])
+@root.route('/listagem-produtos', methods=['GET', 'POST'])
 def criar_produtos():
     # informação apresentada ao cliente
     if request.method == 'GET':
@@ -50,127 +69,115 @@ def criar_produtos():
             produto = Produtos(nome_de_produto=request.form['nome_de_produto'])
             db.session.add(produto)
             db.session.commit()
-            pass
-            return redirect(url_for('criar_produtos'))
+            return render_template('lista_produtos.html')
 
-def db_verificar_email_admin(self):
-        con = sqlite3.connect('database/dados_informacoes.db')
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM admin")
-        emails = cursor.fetchall()
-        con.close()
-        return emails
-
-def db_verificar_email(self):
-        con = sqlite3.connect('database/dados_informacoes.db')
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM usuarios")
-        emails = cursor.fetchall()
-        con.close()
-        return emails
-
-
-@root.route('/', methods=['GET'])
+#Home Page
+@root.route('/', methods=['GET','POST'])
 def home():
-    return render_template('index2.html')
+    log_in = False
+    pass_word = False
+    log_in_admin = False
+    pass_word_admin=False
+
+    if request.method == 'GET':
+        return render_template('index2.html',log_in=log_in, pass_word=pass_word, log_in_admin=log_in_admin, pass_word_admin=pass_word_admin)
+
+    if request.method =='POST':
+        return redirect(url_for('index2.html',log_in=log_in))
+
 
 ##Pagina de LOGIN##
-
-
 @root.route('/user-login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        #Permissoes de login
         log_in_admin = False
         pass_word_admin = False
-        #Check if admin
-        verify_loggin_admin = request.form['email_login']
-        verify_pswd_admin =request.form['password_login']
-        dados_database_admin = "SELECT * FROM admin"
-        #Check if buyer
-        verify_loggin = request.form['email_login']
-        verify_pswd = request.form['password_login']
-        dados_database = "SELECT * FROM usuarios"
-
-        verificado_email_admin = db_verificar_email_admin(dados_database_admin)
-        verificado_email_buyer = db_verificar_email(dados_database)
-        
-        try:
-            for user in verificado_email_admin:
-                if verify_loggin_admin == user[1] and verify_pswd_admin == user[3]: ##erro##
-                    log_in_admin=True  
-                    pass_word_admin = True
-                    return render_template('lista_produtos.html')
-                else:
-                    for user in verificado_email_buyer:
-                        if verify_loggin == user[0] and verify_pswd == user[3]:
-                            log_in = True
-                            pass_word = True
-                            return redirect(url_for('home'))
-                        elif verify_loggin == user[0] and verify_pswd != user[3]:
-                            print('Password errada')
-                    
-                        elif verify_loggin != user[0]:
-                            print('Email nao encontrado')
-                        else:
-                            print('Faca o seu registo.')
-
-        except:
-            for user in verificado_email:
-                if verify_loggin == user[0] and verify_pswd == user[3]:
-                    log_in = True
-                    pass_word = True
-                    return redirect(url_for('home'))
-                elif verify_loggin == user[0] and verify_pswd != user[3]:
-                    print('Password errada')
-                    
-                elif verify_loggin != user[0]:
-                    print('Email nao encontrado')
-                else:
-                    print('Faca o seu registo.')
-            if log_in == True and pass_word == True:
-                print('loggin')
-                return redirect(url_for('home'))
-            else:
-                print('nao loggin')
-                pass
-
-        else:
-            return render_template('login.html')
-
-    if request.method == 'GET':
         log_in = False
         pass_word = False
+        
+        #Input for log in
+        verify_loggin = request.form['email_login']
+        verify_pswd = request.form['password_login']
+
+        #Base de dados User
+        dados_database = "SELECT * FROM usuarios"
+        verificado_email = db_verificar_email(dados_database)
+
+        #Base de dados admin
+        dados_database_admin = "SELECT * FROM admin"
+        verificado_email_admin = db_verificar_email_admin(dados_database_admin)
+        
         try:
-            verify_loggin = request.form['email_login']
-            verify_pswd = request.form['password_login']
-            dados_database = "SELECT * FROM usuarios"
-            verificado_email = db_verificar_email(dados_database)
+            #Verificar admin
+            for user in verificado_email_admin:
+                if verify_loggin == user[1] and verify_pswd == user[3]:
+                    log_in_admin=True  
+                    pass_word_admin = True
+                    return render_template('lista_produtos.html',log_in_admin=log_in_admin,pass_word_admin=pass_word_admin,user=user[1])
+                    
             for user in verificado_email:
                 if verify_loggin == user[0] and verify_pswd == user[3]:
                     log_in = True
                     pass_word = True
-                    return redirect(url_for('home'))
-                elif verify_loggin == user[0] and verify_pswd != user[3]:
+                    return render_template("index2.html",log_in=log_in,user=user)
+                elif verify_loggin == user[1] and verify_pswd != user[3]:
                     print('Password errada')
                     
                 elif verify_loggin != user[0]:
                     print('Email nao encontrado')
+
                 else:
                     print('Faca o seu registo.')
-            if log_in == True and pass_word == True:
-                print('loggin')
-                return redirect(url_for('home'))
-            else:
-                print('nao loggin')
-                pass
+            
 
-        except KeyError as e:
-                pass
-                return render_template('login.html')
+        except:
+            #Verificar buyer
+            for user in verificado_email:
+                if verify_loggin == user[0] and verify_pswd == user[3]:
+                    log_in = True
+                    pass_word = True
+                    
+                elif verify_loggin == user[0] and verify_pswd != user[3]:
+                    print('Password errada')
+                elif verify_loggin != user[0]:
+                    print('Email nao encontrado')
+                else:
+                    print('Faca o seu registo.')
+                
+        finally:
+                
+            #Iniciar como Buyer
+                if log_in == True and pass_word == True:
+                    print('loggin')
+                    return render_template("index2.html",log_in=log_in,user=user[2])
 
-    return render_template('login.html')
+                #Iniciar como admin
+                elif log_in_admin == True and pass_word_admin == True:
+                    print('loggin admin')
+                    return render_template('index2.html',log_in_admin=log_in_admin,pass_word_admin=pass_word_admin,user=user[2])
+         
+    if request.method == 'GET':
+        return render_template('login.html')
+
+#Pagina do carrinho
+@root.route('/user-login/carrinho')
+def carrinho():
+    if request.method =='GET':
+        return render_template('carrinho.html')
+
+    if request.method == 'POST':
+        return redirect(url_for('/user-login/carrinho'))
 
 
+
+#Pagina Exibicao do produto detalhado
+@root.route('/asus-geforce-rtx-3080-rog-strix-oc-lhr-12gb6x',methods=['GET'])
+def asusgeforce_rtx3080():
+    return render_template('produtos.html')
+
+
+#Pagina de Registro de utilizador
 @root.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'GET':
@@ -185,14 +192,6 @@ def sign_up():
         db.session.add(email)
         db.session.commit()
         return redirect(url_for('login'))
-
-    return render_template('sign_up.html')
-
-@root.route('/asus-geforce-rtx-3080-rog-strix-oc-lhr-12gb6x',methods=['GET'])
-def asusgeforce_rtx3080():
-    return render_template('produtos.html')
-
-
 
 db.create_all()
 db.session.commit()
